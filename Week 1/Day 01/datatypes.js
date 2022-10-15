94f5bff5c573583227b1f7fa058ae4409bb2e31d
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2022-10-15 03:01:59 -0800

    First Submission

---
+++ Week 1/Day 01/datatypes.js
@@ -0,0 +1,32 @@
+/*5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. 
+Check the data type of each variable*/
+console.log(typeof "Oluwatoyin");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof lastName);
+
+//6. Declare four variables without assigning values
+let number;
+let date;
+let country;
+let city;
+
+//7. Declare four variables with assigned values
+let typeOfChair = "Sofa";
+let length = 100;
+let day = "Monday";
+let month = "October";
+
+//8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let myFirstname = "Oluwatoyin";
+let myLastname = "Jolaoso";
+let maritalStatus = "Single";
+let age = 24;
+
+//9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let myFirstName = "Oluwatoyin", myLastName = "Jolaoso", myMaritalStatus = "Single", myCurrentAge = 24;
+
+//10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.*/
+let myAge = "I am 24 years old.", yourAge = "You are 30 years old.";
+console.log(myAge);
+console.log(yourAge);
---
+++ Week 1/Day 01/variable.js
@@ -0,0 +1,5 @@
+//4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let firstName = "Oluwatoyin"
+let Oluwatoyin = true
+let lastName;
+let zeroValue = null
