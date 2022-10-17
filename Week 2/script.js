bfa5e8f36605da409ccfc3e0bf4d01e8b0cd5f9a
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2022-10-17 15:21:29 +0100

    Answer to Level 3 questions

--- Week 2/script.js
+++ Week 2/script.js
@@ -129,7 +129,7 @@ const number = Math.floor(Math.random()* (101 - 50) + 50)
 console.log(number)
 
 // Generate a random number between 0 and 255 inclusively.
-const num3 = Math.floor(Math.random() * 255)
+const num3 = Math.floor(Math.random() * 256)
 console.log(num3)
 
 // Access the 'JavaScript' string characters using a random number.
@@ -161,19 +161,26 @@ console.log(phrase.substring(31, 55))
 // 'Love is the best thing in this world. Some found their love and some are still looking for their love.'\
 //Count the number of word love in this sentence.
 let word1 = '/Love is the best thing in this world.\
- Some found their love and some are still looking for their love./gi'
-console.log(word1.match('love'))
+ Some found their love and some are still looking for their love.'
+console.log(word1.match(/love/gi))
 
 // Use match() to count the number of all because in the following sentence:\
 //'You cannot end a sentence with because because because is a conjunction'
-
+let statement2 = `You cannot end a sentence with because because because is a conjunction`
+console.log(statement2.match(/because/gi))
 
 // Clean the following text and find the most frequent word (hint, use replace and regular expressions).
 // const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;.\
 //The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;\
 //I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!?\
 // %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. \
+The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. \
+;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u \
+to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
+console.log(sentence.replace(/[^a-zA-Z .?0-9]/g, ""))
 
 // Calculate the total annual income of the person by extracting the numbers from the following text.\
 //'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+const report = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
